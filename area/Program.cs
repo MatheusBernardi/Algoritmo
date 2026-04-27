1f56311ccddd9c85030c732298df595f266f645b
MatheusBernardi <etecpessoal2010@gmail.com>
2026-04-27 12:29:09 -0300

    novos algoritmos

---
+++ area/Program.cs
@@ -0,0 +1,17 @@
+﻿//algoritmo que calcula a área do círculo
+//declaração das variáveis
+double raio = 0.0;
+double raio = 0.0;
+double pi = 3,14159;
+
+//interação com o usuário
+Console.WriteLine("-----Cálculo da área do círculo-----");
+Console.Write("Informe o valor do raio do círculo");
+raio = convert.ToDouble(Console.ReadLine());
+
+//cálculo da área
+area = pi * (raio * raio);
+
+//exibir o resultado no terminal
+//Interpolação de strings (textos)
+Console.WriteLine($"O valor da área do círculo é {area:F3}");
---
+++ diferenca/Program.cs
@@ -0,0 +1,20 @@
+﻿// variaveis
+int A = 0;
+int B = 0;
+int C = 0;
+int D = 0;
+
+//interação com o usuario
+Console.WriteLine("*** Diferença entre produtos ***");
+
+Console.WriteLine("Digite o primeiro número inteiro");
+A = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número inteiro");
+B = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro número inteiro");
+C = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Digite o quarto número inteiro");
+D = Convert.ToInt16(Console.ReadLine());
